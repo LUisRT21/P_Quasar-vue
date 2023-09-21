4482ab2b72dc156a1351479d328e04d47e6db275
GVides30 <gadielvides@gmail.com>
2023-09-21 16:53:50 -0600

    Menú desplegable para filtrar telefonos

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,14 +1,26 @@
 <template>
-  <q-layout>
-    <q-header>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-btn flat icon="home" to="/" />
+        <q-btn dense flat round icon="ion-md-menu" @click="toggleLeftDrawer" />
         <q-toolbar-title>Tienda CellPhone</q-toolbar-title>
       </q-toolbar>
     </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" side="left" overlay bordered>
+      <div class="q-pa-md q-gutter-lg">
+        <q-toggle v-model="value" color="green" label="Nuevo" />
+      </div>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
     <q-main>
       <router-view />
     </q-main>
+
     <q-footer>
       <q-toolbar>
         <q-btn flat icon="info" to="/about" />
@@ -17,3 +29,24 @@
     </q-footer>
   </q-layout>
 </template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup(props) {
+    const leftDrawerOpen = ref(false);
+    const value = ref(true);
+
+    const toggleLeftDrawer = () => {
+      leftDrawerOpen.value = !leftDrawerOpen.value;
+    };
+
+    return {
+      leftDrawerOpen,
+      value,
+      toggleLeftDrawer,
+    };
+  },
+};
+</script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,6 +6,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
+  {
+    path: '/LayoutDos',
+    component: () => import('layouts/LayoutDos.vue'),
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  },
 
   // Always leave this as last one,
   // but you can also remove it
