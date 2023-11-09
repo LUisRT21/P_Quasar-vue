5e2dc00cc99d726ece6eb2a13f0a1eda945c63c9
LUisRT21 <luisisrael21r@gmail.com>
2023-11-08 20:44:12 -0600

    card terminadas?

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,94 +1,207 @@
 <template>
   <q-page class="row items-center justify-evenly accent">
-    <q-card class="q-card q-mb-md q-hoverable" v-if="$q.screen.width >= 930">
-      <q-card-section>
+    <div class="gt-sm">
+      <q-card class="q-card q-mb-md q-hoverable" v-if="$q.screen.width >= 930">
         <q-card-section>
-          <q-form>
-            <q-card-section horizontal>
-              <q-card-section>
-                <q-input prefix="$" label="Precio:" v-model="precio" outlined />
-              </q-card-section>
-              <q-card-section>
-                <q-input prefix="$" label="hasta:" v-model="hasta" outlined />
-              </q-card-section>
+          <q-card-section>
+            <q-form>
               <q-card-section horizontal>
                 <q-card-section>
-                  <div class="text-h6">Ordenar por:</div>
+                  <q-input
+                    prefix="$"
+                    label="Precio:"
+                    v-model="precio"
+                    outlined
+                  />
                 </q-card-section>
                 <q-card-section>
-                  <div class="q-gutter-sm">
-                    <q-btn color="primary">
-                      <q-icon left size="2em" name="ion-arrow-up" />
-                      <div>Precio</div>
-                    </q-btn>
-                    <q-btn color="primary" label="Fecha" />
-                  </div>
+                  <q-input prefix="$" label="hasta:" v-model="hasta" outlined />
+                </q-card-section>
+                <q-card-section horizontal>
+                  <q-card-section>
+                    <div class="text-h6">Ordenar por:</div>
+                  </q-card-section>
+                  <q-card-section>
+                    <div class="q-gutter-sm">
+                      <q-btn color="primary">
+                        <q-icon left size="2em" name="ion-arrow-up" />
+                        <div>Precio</div>
+                      </q-btn>
+                      <q-btn color="primary" label="Fecha" />
+                    </div>
+                  </q-card-section>
                 </q-card-section>
               </q-card-section>
-            </q-card-section>
-          </q-form>
-        </q-card-section>
-      </q-card-section>
-    </q-card>
-
-    <div class="q-pa-md row items-start q-gutter-md justify-center">
-      <q-card class="q-card q-mb-md q-hoverable" v-if="$q.screen.width < 930">
-        <q-card-section horizontal>
-          <q-card-section>
-            <div col text-left text-weight-bold class="text-subtitle-1 q-pt-lg">
-              Ordenar por:
-            </div>
+            </q-form>
           </q-card-section>
+        </q-card-section>
+      </q-card>
+
+      <div class="q-pa-md row items-start q-gutter-md justify-center">
+        <q-card class="q-card q-mb-md q-hoverable" v-if="$q.screen.width < 930">
+          <q-card-section horizontal>
+            <q-card-section>
+              <div
+                col
+                text-left
+                text-weight-bold
+                class="text-subtitle-1 q-pt-lg"
+              >
+                Ordenar por:
+              </div>
+            </q-card-section>
+            <q-card-section>
+              <q-select
+                filled
+                v-model="multiple"
+                multiple
+                :options="options"
+                style="width: 100%"
+              />
+            </q-card-section>
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div class="contenedor">
+        <q-card
+          v-for="(phone, index) in phonesPaginados"
+          :key="index"
+          v-ripple
+          class="card my-box cursor-pointer q-hoverable"
+          @click="VerDetalles(phone.modelo)"
+        >
+          <span class="q-focus-helper"></span>
+          <!-- Imagen de la tarjeta -->
           <q-card-section>
-            <q-select
-              filled
-              v-model="multiple"
-              multiple
-              :options="options"
-              style="width: 100%"
+            <q-img
+              v-if="phoneImages[index]"
+              :src="phoneImages[index]"
+              class="q-pa-md"
+              style="height: 200px; padding: 10px; overflow: hidden"
             />
           </q-card-section>
-        </q-card-section>
-      </q-card>
+          <!-- Contenido de la tarjeta -->
+          <q-card-section class="card-content">
+            <q-list>
+              <q-list-item>
+                <q-icon name="phone" />
+                <q-label>{{ phone.marca }} {{ phone.modelo }}</q-label>
+              </q-list-item>
+            </q-list>
+            <p class="text-h6 text-weight-bold">${{ phone.precio }}</p>
+            <!-- Accion de la tarrjeta -->
+            <q-card-section>
+              <q-btn
+                color="primary"
+                label="Añadir al carrito"
+                class="q-ma-xs"
+              ></q-btn>
+            </q-card-section>
+          </q-card-section>
+        </q-card>
+      </div>
     </div>
-
-    <div class="q-pa-md row items-start q-gutter-md justify-center">
-      <q-card
-        v-for="(phone, index) in phonesPaginados"
-        :key="index"
-        v-ripple
-        class="q-card q-mb-xs col-12 col-sm-6 col-md-3 col-lg-2 my-box cursor-pointer q-hoverable"
-        @click="VerDetalles(phone.modelo)"
-      >
-        <span class="q-focus-helper"></span>
-        <!-- Imagen de la tarjeta -->
+    <div class="lt-md">
+      <q-card class="q-card q-mb-md q-hoverable" v-if="$q.screen.width >= 930">
         <q-card-section>
-          <q-img
-            v-if="phoneImages[index]"
-            :src="phoneImages[index]"
-            class="q-pa-md"
-            style="height: 200px; padding: 10px; overflow: hidden"
-          />
-        </q-card-section>
-        <!-- Contenido de la tarjeta -->
-        <q-card-section class="card-content">
-          <q-list class="caracteristicas-verticales">
-            <q-list-item>
-              <q-icon name="phone" />
-              <q-label>{{ phone.marca }} {{ phone.modelo }}</q-label>
-            </q-list-item>
-          </q-list>
-          <p class="text-h6 text-weight-bold">${{ phone.precio }}</p>
-          <!-- Accion de la tarrjeta -->
           <q-card-section>
-            <q-btn
-              color="primary"
-              label="Añadir al carrito"
-              class="q-ma-xs"
-            ></q-btn>
+            <q-form>
+              <q-card-section horizontal>
+                <q-card-section>
+                  <q-input
+                    prefix="$"
+                    label="Precio:"
+                    v-model="precio"
+                    outlined
+                  />
+                </q-card-section>
+                <q-card-section>
+                  <q-input prefix="$" label="hasta:" v-model="hasta" outlined />
+                </q-card-section>
+                <q-card-section horizontal>
+                  <q-card-section>
+                    <div class="text-h6">Ordenar por:</div>
+                  </q-card-section>
+                  <q-card-section>
+                    <div class="q-gutter-sm">
+                      <q-btn color="primary">
+                        <q-icon left size="2em" name="ion-arrow-up" />
+                        <div>Precio</div>
+                      </q-btn>
+                      <q-btn color="primary" label="Fecha" />
+                    </div>
+                  </q-card-section>
+                </q-card-section>
+              </q-card-section>
+            </q-form>
           </q-card-section>
         </q-card-section>
       </q-card>
+
+      <div class="q-pa-md row items-start q-gutter-md justify-center">
+        <q-card class="q-card q-mb-md q-hoverable" v-if="$q.screen.width < 930">
+          <q-card-section horizontal>
+            <q-card-section>
+              <div
+                col
+                text-left
+                text-weight-bold
+                class="text-subtitle-1 q-pt-lg"
+              >
+                Ordenar por:
+              </div>
+            </q-card-section>
+            <q-card-section>
+              <q-select
+                filled
+                v-model="multiple"
+                multiple
+                :options="options"
+                style="width: 100%"
+              />
+            </q-card-section>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="contenedor-chi">
+        <q-card
+          v-for="(phone, index) in phonesPaginados"
+          :key="index"
+          v-ripple
+          class="card-chi my-box cursor-pointer q-hoverable"
+          @click="VerDetalles(phone.modelo)"
+        >
+          <span class="q-focus-helper"></span>
+          <!-- Imagen de la tarjeta -->
+          <q-card-section>
+            <q-img
+              v-if="phoneImages[index]"
+              :src="phoneImages[index]"
+              class="q-pa-md"
+              style="height: 200px; padding: 10px; overflow: hidden"
+            />
+          </q-card-section>
+          <!-- Contenido de la tarjeta -->
+          <q-card-section class="card-content">
+            <q-list>
+              <q-list-item>
+                <q-icon name="phone" />
+                <q-label>{{ phone.marca }} {{ phone.modelo }}</q-label>
+              </q-list-item>
+            </q-list>
+            <p class="text-h6 text-weight-bold">${{ phone.precio }}</p>
+            <!-- Accion de la tarrjeta -->
+            <q-card-section>
+              <q-btn
+                color="primary"
+                label="Añadir al carrito"
+                class="q-ma-xs"
+              ></q-btn>
+            </q-card-section>
+          </q-card-section>
+        </q-card>
+      </div>
     </div>
     <q-pagination
       v-model="PaginaActual"
